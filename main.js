19dc98552b1f5962a7e61a8a8870d2b245b7ddaa
LAW <lawsiufung@me.com>
2017-08-09 17:42:44 +0800

    just stuffs

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $(document).ready( function() {
 							0]
 							
 	var abilityNameList = [	"Ability_Ability_Doubler",		"Ability_Bomb_Defense_Up",	"Ability_Cold_Blooded",			"Ability_Comeback",				"Ability_Drop_Roller",
-							"Ability_Haunt",				"Ability_Ink_Recovery_Up",	"Ability_Ink_Resistance_Up",	"Ability_Ink_Saver_Main",		"Ability_Ink_Saver_Sub",
+							"Ability_Haunt",				"Ability_Ink_Recovery_Up",	"Ability Ink Resistance Up",	"Ability_Ink_Saver_Main",		"Ability_Ink_Saver_Sub",
 							"Ability_Last-Ditch_Effort",	"Ability_Ninja_Squid",		"Ability_Object_Shredder",		"Ability_Opening_Gambit",		"Ability_Quick_Respawn",
 							"Ability_Quick_Super_Jump",		"Ability_Respawn_Punisher",	"Ability_Run_Speed_Up",			"Ability_Special_Charge_Up",	"Ability_Special_Power_Up",
 							"Ability_Special_Saver",		"Ability_Stealth_Jump",		"Ability_Sub_Power_Up",			"Ability_Swim_Speed_Up",		"Ability_Tenacity",
@@ -57,6 +57,9 @@ $(document).ready( function() {
 		}
 	}
 	
+	// function filterGearByAbility( body, ability ) {
+		// output string of 
+	
 	// gear constructor
 	function gear( name, body, brand, price, ability, rarity) {
 		this.name = name;
@@ -84,7 +87,7 @@ $(document).ready( function() {
 	
 	// gearList.push( new gear( ) );
 	
-	
+	/*
 	// toy example
 	var wantedAbility = "Opening Gambit";
 	var wantedBrand = "Annaki";
@@ -94,32 +97,19 @@ $(document).ready( function() {
 				el.ability === wantedAbility;
 	});
 	$("#list3").text( JSON.stringify( filteredArray ) );
-	
-	// Fade in
-	/*
-	$(document).hide();
-	$(document).fadeIn("slow");
 	*/
-	
+
+
 	// Hide all gears;
-	initVisibleGear();
+	// initVisibleGear();
 	
 	// On clicking ability icons
 	$(".ability").on("click", function() {
-		$(this).toggleClass("highlightA")
-			
+		$(this).toggleClass("highlightA");
 		var selectedIndex = abilityNameList.indexOf( $(this).attr("id") );
 		abilityCheckList[selectedIndex] = ( abilityCheckList[selectedIndex] + 1 ) % 2;
 		visualCheckList();
 				
 	});
 	
-	$(".brand").on("click", function() {
-		$(this).toggleClass("highlightB")
-		var selectedIndex = brandNameList.indexOf( $(this).attr("id") );
-		brandCheckList[selectedIndex] = ( brandCheckList[selectedIndex] + 1 ) % 2;
-	
-		visualCheckList();
-		
-	});
 });
